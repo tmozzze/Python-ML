da6832e6030924970d6e38819c0276078bf7eb72
Istomin Alexey <tmozzze@gmail.com>
2024-08-02 13:59:24 +0300

    1.3
    Added metrics

--- LogRegression/LogRegression.py
+++ LogRegression/LogRegression.py
@@ -4,10 +4,13 @@ import pandas as pd
 
 class MyLogReg():
 
-    def __init__(self, n_iter=10, learning_rate=0.1, weights=None):
+    def __init__(self, n_iter=10, learning_rate=0.1, weights=None, metric=None):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
+        self.metric = metric
+
+        self.best_score = None
 
 
     def __str__(self):
@@ -25,6 +28,52 @@ class MyLogReg():
         log_loss = -np.mean(y * np.log(y_hat + eps) + (1 - y) * np.log(1 - y_hat + eps))
         return log_loss
 
+    def compute_roc_auc(self, y, y_hat):
+        sorted_idx = np.argsort(y_hat)[::-1]
+        y_true_sorted = y[sorted_idx]
+
+        tps = np.cumsum(y_true_sorted)
+        fps = np.cumsum(1 - y_true_sorted)
+
+        tpr = tps / np.sum(y_true_sorted)
+        fpr = fps / np.sum(1 - y_true_sorted)
+
+        # Добавим начальные и конечные точки (0, 0) и (1, 1)
+        tpr = np.concatenate([[0], tpr, [1]])
+        fpr = np.concatenate([[0], fpr, [1]])
+
+        auc = np.trapz(tpr, fpr)
+        return round(auc, 10)
+
+
+    def compute_metric(self, y, y_hat):
+        if self.metric is not None:
+            y_pred = (y_hat >= 0.5).astype(int)
+
+            tp = np.sum((y_pred == 1) & (y == 1))
+            fp = np.sum((y_pred == 1) & (y == 0))
+            tn = np.sum((y_pred == 0) & (y == 0))
+            fn = np.sum((y_pred == 0) & (y == 1))
+
+            accuracy = (tp + tn) / (tp + tn + fp + fn)
+            precision = tp / (tp + fp)
+            recall = tp / (tp + fn)
+            f1 = 2 * precision * recall / (precision + recall)
+
+            if self.metric == "accuracy":
+                return accuracy
+            if self.metric == "precision":
+                return precision
+            if self.metric == "recall":
+                return recall
+            if self.metric == "f1":
+                return f1
+            if self.metric == "roc_auc":
+                return self.compute_roc_auc(y, y_hat)
+            else:
+                return None
+
+
     def fit(self, X, y, verbose=False):
         X = np.c_[np.ones(X.shape[0]), X]
 
@@ -37,16 +86,30 @@ class MyLogReg():
 
             loss = self.compute_log_loss(y, y_hat)
 
+
             if verbose and (i % verbose == 0):
-                print(f"{i if i > 0 else 'start'} | loss: {loss:.2f} | learning rate: {self.learning_rate}")
+                if self.metric is not None:
+                    metric_value = self.compute_metric(y, y_hat)
+                    print(f"{i if i > 0 else 'start'} | loss: {loss:.2f} | learning rate: {self.learning_rate} | {self.metric}: {metric_value}")
+
+                else:
+                    print(f"{i if i > 0 else 'start'} | loss: {loss:.2f} | learning rate: {self.learning_rate}")
 
             error = y_hat - y
             gradient = np.dot(error, X) / len(y)
 
             self.weights -= self.learning_rate * gradient
 
+        y_hat = self.sigmoid(np.dot(X, self.weights))
+
         if verbose:
-            print(f"final | loss: {loss:.2f} | learning rate: {self.learning_rate}")
+            if self.metric is not None:
+                metric_value = self.compute_metric(y, y_hat)
+                print(f"final | loss: {loss:.2f} | learning rate: {self.learning_rate} | {self.metric}: {metric_value}")
+            else:
+                print(f"final | loss: {loss:.2f} | learning rate: {self.learning_rate}")
+
+        self.best_score = self.compute_metric(y, y_hat)
 
     def predict_proba(self, X):
         X = np.c_[np.ones(X.shape[0]), X]
@@ -61,6 +124,9 @@ class MyLogReg():
     def get_coef(self):
         return self.weights[1:]
 
+    def get_best_score(self):
+        return self.best_score
+
 
 from sklearn.datasets import make_classification
 
@@ -71,18 +137,21 @@ X.columns = [f'col_{col}' for col in X.columns]
 
 
 
-model = MyLogReg(n_iter=100, learning_rate=0.1)
+model = MyLogReg(n_iter=200, learning_rate=0.1, metric="roc_auc")
 
 print(model)
 
 model.fit(X, y, 10)
 
-print(model.get_coef())
+print("coef: ",model.get_coef())
+
+print("best score: ", model.get_best_score())
 
 X_test, y_test = make_classification(n_samples=400, n_features=14, n_informative=10, random_state=42)
 X_test = pd.DataFrame(X_test)
 y_test = pd.Series(y_test)
 X_test.columns = [f'col_{col}' for col in X_test.columns]
 
-print("predict: ", model.predict(X_test))
-print("predict proba: ", model.predict_proba(X_test))
+
+print("predict: ", np.sum(model.predict(X_test)))
+print(f"predict proba: {np.mean(model.predict_proba(X_test)):.2f}")

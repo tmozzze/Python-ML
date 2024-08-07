8c10a3d6131695b2f5f454b21451781fe94b7271
Istomin Alexey <tmozzze@gmail.com>
2024-08-07 20:32:25 +0300

    Update KNNClassification.py
    Optimized code

--- KNNClassification/KNNClassification.py
+++ KNNClassification/KNNClassification.py
@@ -21,75 +21,45 @@ class MyKNNClf():
         self.y_train = y_train.copy()
         self.train_size = X.shape
 
-    def _euclidean_predict(self, row: pd.Series):
-        mean = self._euclidean_proba(row)
-        if mean >= 0.5:
-            return 1
-        return 0
-
     def _euclidean_proba(self, row: pd.Series):
         k_sorted_idx = (row - self.X_train).pow(2).sum(axis=1).pow(.5).sort_values().head(self.k).index
         return self.y_train[k_sorted_idx].mean()
-
-    def _chebyshev_predict(self, row: pd.Series):
-        mean = self._chebyshev_proba(row)
-        if mean >= 0.5:
-            return 1
-        return 0
 
     def _chebyshev_proba(self, row: pd.Series):
         k_sorted_idx = (row - self.X_train).abs().max(axis=1).sort_values().head(self.k).index
         return self.y_train[k_sorted_idx].mean()
 
-    def _manhattan_predict(self, row: pd.Series):
-        mean = self._manhattan_proba(row)
-        if mean >= 0.5:
-            return 1
-        return 0
-
     def _manhattan_proba(self, row: pd.Series):
         k_sorted_idx = (row - self.X_train).abs().sum(axis=1).sort_values().head(self.k).index
         return self.y_train[k_sorted_idx].mean()
-
-    def _cosine_predict(self, row: pd.Series):
-        mean = self._cosine_proba(row)
-        if mean >= 0.5:
-            return 1
-        return 0
 
     def _cosine_proba(self, row: pd.Series):
         k_sorted_idx = np.argsort(1 - (np.dot(self.X_train, row) / (np.linalg.norm(self.X_train, axis=1) * (np.linalg.norm(row)))))[:self.k]
         return self.y_train.iloc[k_sorted_idx].mean()
 
+    def _predict_proba(self, row: pd.Series):
+        if self.metric == "euclidean":
+            return self._euclidean_proba(row)
+        if self.metric == "chebyshev":
+            return self._chebyshev_proba(row)
+        if self.metric == "manhattan":
+            return self._manhattan_proba(row)
+        if self.metric == "cosine":
+            return self._cosine_proba(row)
+        else:
+            raise ValueError("Unsupported metric: {}".format(self.metric))
 
+    def _predict(self, row: pd.Series):
+        mean = self._predict_proba(row)
+        return 1 if mean >= 0.5 else 0
+
+    def predict_proba(self, X_test: pd.DataFrame):
+        probabilities = X_test.apply(self._predict_proba, axis=1)
+        return probabilities
 
     def predict(self, X_test: pd.DataFrame):
-        if self.metric == "euclidean":
-            predictions = X_test.apply(self._euclidean_predict, axis=1)
-            return predictions
-        elif self.metric == "chebyshev":
-            predictions = X_test.apply(self._chebyshev_predict, axis=1)
-            return predictions
-        elif self.metric == "manhattan":
-            predictions = X_test.apply(self._manhattan_predict, axis=1)
-            return predictions
-        elif self.metric == "cosine":
-            predictions = X_test.apply(self._cosine_predict, axis=1)
-            return predictions
-    def predict_proba(self, X_test: pd.DataFrame):
-        if self.metric == "euclidean":
-            probabilities = X_test.apply(self._euclidean_proba, axis=1)
-            return probabilities
-        elif self.metric == "chebyshev":
-            probabilities = X_test.apply(self._chebyshev_proba, axis=1)
-            return probabilities
-        elif self.metric == "manhattan":
-            probabilities = X_test.apply(self._manhattan_proba, axis=1)
-            return probabilities
-        elif self.metric == "cosine":
-            probabilities = X_test.apply(self._cosine_proba, axis=1)
-            return probabilities
-
+        predictions = X_test.apply(self._predict, axis=1)
+        return predictions
 
 
 
@@ -102,7 +72,7 @@ X = pd.DataFrame(X)
 y = pd.Series(y)
 X.columns = [f'col_{col}' for col in X.columns]
 
-model = MyKNNClf(k=5)
+model = MyKNNClf(k=5, metric="cosine")
 print(model)
 model.fit(X, y)
 print(model)
